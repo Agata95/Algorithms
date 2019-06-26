2ca9fbc8784a016624fa31e00720cb44bf6270aa
Agata <agata.weltrowska@outlook.com>
2019-06-26 14:27:21 +0200

    corrected

--- src/main/java/com/javagda25/silniaRekurencyjnie.java
+++ src/main/java/com/javagda25/silniaRekurencyjnie.java
@@ -2,14 +2,14 @@ package com.javagda25;
 
 public class silniaRekurencyjnie {
     public static void main(String[] args) {
-        System.out.println(recursiveFactorial(5));
+        System.out.println(recursiveFactorial(1));
     }
 
     static int recursiveFactorial(int number) {
-        if (number != 1) {
+        if (number > 1) {
             return recursiveFactorial(number - 1) * number;
         } else {
-            return number;
+            return 0;
         }
     }
 }
